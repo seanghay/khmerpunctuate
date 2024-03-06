c49a9b9a50c75ecfc0ef21e75fad3ec04f3f57ed
Seanghay Yath <seanghay.dev@gmail.com>
2024-03-06 22:45:08 +0700

    doc: readme.md

--- tests/tag.py
+++ tests/tag.py
@@ -2,13 +2,15 @@ from khmernormalizer import normalize
 from khmercut import tokenize
 from khmerpunctuate import punctuate
 
-text = normalize(
-  "អយ្យការអមសាលាដំបូងរាជធានីភ្នំពេញបានព្រមានថានឹងចេញដីកាបញ្ជាឲ្យបង្ខំនិងឲ្យឃុំខ្លួនតាមនីតិវិធីប្រសិនបើលោករ៉ុងឈុនដែលបច្ចុប្បន្នជាទីប្រឹក្សាគណបក្សកម្លាំងជាតិមិនបានបង់ប្រាក់ពិន័យចំនួន២លានរៀលឲ្យបានមុនថ្ងៃទី០៤ខែមីនាឆ្នាំ២០២៤ទេនោះ"
-)
+text = normalize("អយ្យការអមសាលាដំបូងរាជធានីភ្នំពេញបានព្រមានថានឹងចេញដីកាបញ្ជាឲ្យបង្ខំនិងឲ្យឃុំខ្លួនតាមនីតិវិធីប្រសិនបើលោករ៉ុងឈុនដែលបច្ចុប្បន្នជាទីប្រឹក្សាគណបក្សកម្លាំងជាតិមិនបានបង់ប្រាក់ពិន័យចំនួន២លានរៀលឲ្យបានមុនថ្ងៃទី០៤ខែមីនាឆ្នាំ២០២៤ទេនោះ")
 tokens = tokenize(text)
 
 output_text = ""
-for token, punctuation, punctuation_id in punctuate(tokens):
-  output_text += token + punctuation
+for token, punct, punct_id in punctuate(tokens):
+  # exclude special tokens like I-NUMBER, B-NUMBER, I-QOUTE and B-QOUTE
+  if punct_id < 7:
+    output_text += token + punct
+  else:
+    output_text += token
 
 print(output_text)
